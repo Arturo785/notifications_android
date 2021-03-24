e221bb170f4d8965dbe91498c8818dd8f1fc6a8c
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2021-03-23 18:00:20 -0600

    Addition of big image, buttons and media style

--- app/src/main/java/com/example/notificationstutorial/MainActivity.java
+++ app/src/main/java/com/example/notificationstutorial/MainActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 import android.widget.EditText;
 
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTexMessage;
 
+    private MediaSessionCompat mediaSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTexMessage = findViewById(R.id.edit_text_message);
+
+        mediaSession = new MediaSessionCompat(this, "tag");
     }
 
     public void sendOnChannel1(View view) {
@@ -56,18 +61,19 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.kemonito);
+        Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.kemonito);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setLargeIcon(largeIcon)
+                .setLargeIcon(picture)
                 // style part
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(getString(R.string.long_dummy_text))
-                        .setBigContentTitle("Big content title")
-                        .setSummaryText("Summary text"))
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(picture)
+                        .bigLargeIcon(null)
+
+                )
                 // end of style
                 .setPriority(NotificationCompat.PRIORITY_HIGH) // additionally in here for lower api
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
@@ -75,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true) // dismiss the notification when touched
                 .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher, "Action", actionIntent)
+              //  .addAction(R.mipmap.ic_launcher, "Action", actionIntent)
                 .build();
 
         notificationManagerC.notify(id_1, notification);
@@ -87,19 +93,20 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTexMessage.getText().toString();
 
+        Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.kemonito);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_baseline_monetization_on_24)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setStyle(new NotificationCompat.InboxStyle()
-                        .addLine("This is line 1")
-                        .addLine("This is line 2")
-                        .addLine("This is line 3")
-                        .addLine("This is line 4")
-                        .addLine("This is line 5")
-                        .addLine("This is line 6")
-                        .setBigContentTitle("Big content title")
-                        .setSummaryText("Summary text"))
+                .setLargeIcon(artwork)
+                .addAction(R.drawable.hand, "Hi", null)
+                .addAction(R.drawable.cycle, "Run", null)
+                .addAction(R.drawable.call, "Call", null)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0,1,2)
+
+                ).setSubText("Sub text")
                 .setPriority(NotificationCompat.PRIORITY_LOW) // additionally in here for lower api
                 .build();
 
